ff2d30387c5770dfe84b9e9ed0d6feeb75733f5f
DreamMaoMao <2523610504@qq.com>
2026-09-12 22:39:40 +0800

    opt: rmove useless code

--- src/main.c
+++ src/main.c
@@ -116,7 +116,6 @@
 #include <wlr/types/wlr_xdg_shell.h>
 #include <wlr/util/log.h>
 #include <wlr/util/region.h>
-#include <wordexp.h>
 #include <xkbcommon/xkbcommon.h>
 #ifdef XWAYLAND
 #include <X11/Xlib.h>
