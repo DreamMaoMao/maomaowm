65495431c5aedffe723811bfaeb2c02116f65a3a
Brandon Presley <brandon@presley.nz>
2026-09-12 17:06:47 +1200

    fix: ignore xdg geometry offsets when checking dirty state

--- src/manage/client.c
+++ src/manage/client.c
@@ -2201,8 +2201,7 @@ void handle_client_commit(struct wl_listener *listener, void *data) {
 	if (!c->dirty) {
 		new_geo = &c->surface.xdg->geometry;
 		c->dirty = new_geo->width != c->geom.width - 2 * c->bw ||
-				   new_geo->height != c->geom.height - 2 * c->bw ||
-				   new_geo->x != 0 || new_geo->y != 0;
+				   new_geo->height != c->geom.height - 2 * c->bw;
 	}
 
 	if (c == server.grab_client || !c->dirty)
@@ -2212,8 +2211,7 @@ void handle_client_commit(struct wl_listener *listener, void *data) {
 
 	new_geo = &c->surface.xdg->geometry;
 	c->dirty = new_geo->width != c->geom.width - 2 * c->bw ||
-			   new_geo->height != c->geom.height - 2 * c->bw ||
-			   new_geo->x != 0 || new_geo->y != 0;
+			   new_geo->height != c->geom.height - 2 * c->bw;
 }
 
 void handle_client_unmap(struct wl_listener *listener, void *data) {
